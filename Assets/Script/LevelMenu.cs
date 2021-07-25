0787b5b9b9acb893e3629037b53793ba0a436e38
Murran <nfmurran@staff.sfcerenity.org>
2021-07-25 13:35:19 +0700

    Add Main Menu & Pause

---
+++ Assets/Script/LevelMenu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelMenu : MonoBehaviour
+{
+    public void Level_1()
+    {
+        SceneManager.LoadScene("Level1");
+        Time.timeScale = 1f;
+        AudioListener.volume = 1f;
+    }
+
+    public void Level_2()
+    {
+        SceneManager.LoadScene("Level2");
+        Time.timeScale = 1f;
+        AudioListener.volume = 1f;
+    }
+
+    public void Level_3()
+    {
+        SceneManager.LoadScene("Level3");
+        Time.timeScale = 1f;
+        AudioListener.volume = 1f;
+    }
+}
---
+++ Assets/Script/PauseMenu.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    public GameObject Pemain;
+    public GameObject Orang;
+    public GameObject Orang1;
+    public GameObject Orang2;
+    public GameObject Orang3;
+    public GameObject Orang4;
+    public GameObject Orang5;
+    public GameObject joycon;
+
+    public void TogglePause()
+    {
+        Pemain.SetActive(false);
+        Orang.SetActive(false);
+        Orang1.SetActive(false);
+        Orang2.SetActive(false);
+        Orang3.SetActive(false);
+        Orang4.SetActive(false);
+        Orang5.SetActive(false);
+        joycon.SetActive(false);
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        AudioListener.volume = 0f;
+    }
+
+    public void Resume()
+    {
+        Pemain.SetActive(true);
+        Orang.SetActive(true);
+        Orang1.SetActive(true);
+        Orang2.SetActive(true);
+        Orang3.SetActive(true);
+        Orang4.SetActive(true);
+        Orang5.SetActive(true);
+        joycon.SetActive(true);
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        AudioListener.volume = 1f;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("Level1");
+        Time.timeScale = 1f;
+        AudioListener.volume = 1f;
+    }
+
+    public void Exit()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
