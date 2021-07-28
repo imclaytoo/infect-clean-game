61e343c4cdddc0938968afb90a6c3cfa6b193371
erikokres <erikokresna@gmail.com>
2021-07-29 01:15:36 +0700

    add Fail Scorring

--- Assets/Script/People.cs
+++ Assets/Script/People.cs
@@ -17,6 +17,7 @@ public class People : MonoBehaviour
     void Start()
     {
         playerPos = player.GetComponent<Transform>();
+        score = 0;
     }
 
     // Update is called once per frame
@@ -25,7 +26,7 @@ public class People : MonoBehaviour
         if (Vector2.Distance(transform.position, playerPos.position) < distance)
         {
             buttonAction.SetActive(true);
-            score = +1;
+            
         }
         else
         {
@@ -36,5 +37,7 @@ public class People : MonoBehaviour
     public void ChangeMask(){
         this.gameObject.GetComponent<SpriteRenderer>().sprite = mask;
         Destroy(gameObject, 2f);
+        score += 1;
+        Debug.Log(score);
     }
 }
---
+++ Assets/Script/Scoring.cs
@@ -0,0 +1,34 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoring : MonoBehaviour
+{
+    public GameObject scoreButton;
+
+    public float score;
+
+    void Start() {
+        score = 0;   
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag.Equals("People"))
+        {
+        scoreButton.SetActive(true);
+        }    
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        if (other.gameObject.tag.Equals("People"))
+        {
+            scoreButton.SetActive(false);
+        }    
+    }
+
+    public void AddScore(){
+        score += 1;
+        Debug.Log(score);
+    }
+}
